93605a4a66f309fa57c0e97278fdd626e3192c60
xiangyi.zhu <xiangyi.zhu@opay.com>
2025-10-15 16:40:36 +0800

    add

--- be/src/exec/hdfs_scanner/jni_scanner.cpp
+++ be/src/exec/hdfs_scanner/jni_scanner.cpp
@@ -543,11 +543,12 @@ std::unique_ptr<JniScanner> create_hive_jni_scanner(const JniScanner::CreateOpti
     jni_scanner_params["input_format"] = input_format;
     jni_scanner_params["time_zone"] = time_zone;
     jni_scanner_params["fs_options_props"] = build_fs_options_properties(*(options.fs_options));
-    if (scan_range.__isset.text_file_desc &&
-        scan_range.text_file_desc.__isset.skip_header_line_count) {
+    if (scan_range.__isset.text_file_desc && scan_range.text_file_desc.__isset.skip_header_line_count) {
+        const auto& text_file_desc = scan_range.text_file_desc;
         jni_scanner_params["skip.header.line.count"] =
-            std::to_string(scan_range.text_file_desc.skip_header_line_count);
+        std::to_string(text_file_desc.skip_header_line_count);
     }
+
     for (const auto& pair : serde_properties) {
         jni_scanner_params[serde_property_prefix + pair.first] = pair.second;
     }
